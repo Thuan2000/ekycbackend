7750885d6c4c0d646b6ad20de5a7190c2834956b
Thuan Nguyen <thuan.nguyen@eyeq.tech>
2019-05-31 13:26:29 +0700

    Progress

--- ems/base_server.py
+++ ems/base_server.py
@@ -10,9 +10,9 @@ class AbstractServer():
 
         @app.after_request
         def after_request(response):
-            response.headers.add('Access-Control-Allow-Origin', '*')
-            response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
-            response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+            # response.headers.add('Access-Control-Allow-Origin', '*')
+            # response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+            # response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
             return response
 
         self.app = app
